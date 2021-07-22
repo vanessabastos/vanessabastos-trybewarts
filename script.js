72bb082e35cbf7a710b9352d213a6517c5cdeb98
Dayane <dayanebmartins@gmail.com>
2021-07-22 13:04:40 -0300

    corrige identação

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
 const btnEntrar = document.getElementById('entrar');
 
-btnEntrar.addEventListener('click', function (event){
+btnEntrar.addEventListener('click', function (event) {
   event.preventDefault();
   let email = inputEmail.value;
-  let senha=inputSenha.value;
-  if(email !=='tryber@teste.com' || senha!=='123456'){
+  let senha = inputSenha.value;
+  if (email !== 'tryber@teste.com' || senha !== '123456') {
     alert("Login ou senha inválidos");
   }
-  else{
-    alert( "Olá, Tryber!")
+  else {
+    alert("Olá, Tryber!")
   }
-   
-  }
+
+}
 )
