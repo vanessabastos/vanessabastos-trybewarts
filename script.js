ffa06dc688e7e426c36632731371970a83bfa748
Dayane <dayanebmartins@gmail.com>
2021-07-22 21:13:24 -0300

    corrigindo identação

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ btnEntrar.addEventListener('click', (event) => {
   event.preventDefault();
   if (inputEmail.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
     alert('Login ou senha inválidos.');
-  }
-  else {
+  } else {
     alert('Olá, Tryber!');
   }
-}
-);
+});
