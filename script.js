dc875fdeccf9c8b802eaec482e978147b33929c0
vanessabastos <vanessarsb83@gmail.com>
2021-07-24 15:38:08 -0300

    corrigindo CSS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
 const btnEntrar = document.getElementById('entrar');
+const btnEnviar = document.getElementById('submit-btn');
+const inputConcorda = document.getElementById('agreement');
 
 btnEntrar.addEventListener('click', (event) => {
   event.preventDefault();
@@ -10,3 +12,13 @@ btnEntrar.addEventListener('click', (event) => {
     alert('Olá, Tryber!');
   }
 });
+
+btnEnviar.addEventListener('click', () => {
+  if (inputConcorda.checked === true) {
+    btnEnviar.disabled = false;
+  } else { 
+    btnEnviar.disabled = true;
+  }
+});
+
+console.log(btnEnviar);
